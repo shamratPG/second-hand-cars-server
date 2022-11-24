cd839d6c31474e89e03bb1dc26edb2ffa89f5ebb
Md. Shamrat Hossain <shamratpg@gmail.com>
2022-11-25 00:46:21 +0600

    added users api

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pra9xm3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const database = client.db('secondHandCars');
+        const userCollection = database.collection('userCollection');
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await userCollection.find(query).toArray();
+            res.send(users)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
 
 
 app.get('/', async (req, res) => {
