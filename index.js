ee6634c719cd22522b43b76c18609c409251561c
Md. Shamrat Hossain <shamratpg@gmail.com>
2022-11-27 15:50:17 +0600

    added product API

--- index.js
+++ index.js
@@ -18,15 +18,24 @@ async function run() {
     try {
         const database = client.db('secondHandCars');
         const userCollection = database.collection('userCollection');
+        const productCollection = database.collection('productCollection');
 
+        // Get all users 
         app.get('/users', async (req, res) => {
-            const role = req.query.role;
-            const query = { role: role };
+            const query = {};
             const users = await userCollection.find(query).toArray();
             res.send(users)
         })
 
-        // Checks if user already exist
+        // Get all user by their role
+        app.get('/users', async (req, res) => {
+            const role = req.query.role;
+            const query = { role };
+            const users = await userCollection.find(query).toArray();
+            res.send(users)
+        })
+
+        // Get single user by email
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email };
@@ -34,19 +43,40 @@ async function run() {
             res.send(user)
         })
 
+        // Add New User
         app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user?.email }
-            console.log(query)
+
             const alreadyUser = await userCollection.find(query).toArray();
-            console.log(alreadyUser.length)
             if (alreadyUser.length) {
                 return res.send({ acknowledged: false });
             }
             const newUser = await userCollection.insertOne(user);
-            console.log(newUser)
             res.send(newUser);
         })
+
+
+        // Get Products (by categories also)
+        app.get('/products', async (req, res) => {
+            const category = req?.query?.category;
+            console.log(category);
+            if (category) {
+                const query = { category };
+                const products = await productCollection.find(query).toArray();
+                return res.send(products)
+            }
+            const query = {};
+            const products = await productCollection.find(query).toArray();
+            res.send(products)
+        })
+
+        // Add New Product
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        });
     }
     finally {
 
